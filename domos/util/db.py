ffc3597f8b3a67d7f65d0c2be4c148fb9d180245
bergzand <hydrazine@bergzand.net>
2014-09-11 14:35:21 +0200

    Documentation added to most functions

--- domos/util/db.py
+++ domos/util/db.py
@@ -2,14 +2,23 @@
 from peewee import *
 import datetime
 import math
-import statistics
+try:
+    import statistics
+except:
+    statistics = None
 
 dbconn = Proxy()
 
 rpctypes = ['list', 'get', 'del', 'add', 'set']
 
+"""
+Peewee database models
+"""
+
 
 class BaseModel(Model):
+    """Base model of the database, provides default settings
+    """
     descr = TextField(null=True)
 
     class Meta:
@@ -21,22 +30,45 @@ class BaseModel(Model):
 
 
 class Module(BaseModel):
+    """Model of a module
+
+    * Name: Name of the module
+    * queue: message queue the module is listening on
+    * Active: whether the module is active or not
+
+    """
     name = CharField()
     queue = CharField()
     Active = BooleanField()
 
 
 class RPCTypes(BaseModel):
+    """Available types of RPC calls
+    see also the rpctypes variable
+    """
     rpctype = CharField()
 
 
 class ModuleRPC(BaseModel):
+    """RPC a module supports. 
+
+    * Module: foreign key to a module
+    * RPCType: foreign, the type of rpc
+    * key: Key to distiguish multiple rpc's of the same type
+    """
     Module = ForeignKeyField(Module, on_delete='CASCADE')
     RPCType = ForeignKeyField(RPCTypes)
     Key = CharField()
 
 
 class RPCArgs(BaseModel):
+    """Arguments for a RPC
+
+    * ModuleRPC: foreign key to a RPC
+    * name: Name of the argument, used in the dictionary send to the module
+    * RPCargtype: Type of argument, eg: string, integer
+    * Optional: Whether the argument is optional
+    """
     ModuleRPC = ForeignKeyField(ModuleRPC, on_delete='CASCADE', related_name='args')
     name = CharField()
     RPCargtype = CharField()
@@ -44,12 +76,27 @@ class RPCArgs(BaseModel):
 
 
 class Sensors(BaseModel):
+    """Sensor
+    
+    * Module: Module associated with the sensor
+    * Ident: identifier of the sensor
+    * Active: Whether the sensor is active or disabled
+    * Instant: is the sensor of the type Instant
+    """
     Module = ForeignKeyField(Module, on_delete='CASCADE')
     ident = CharField()
     Active = BooleanField(default=True)
     Instant = BooleanField(default=False)
 
+
 class SensorValues(BaseModel):
+    """Values of a sensor, represents a measurement value 
+    of the sensor at a certain point in time
+
+    * Sensor: associated sensor
+    * Value: measurement value
+    * Timestamp: Point in time of the value
+    """
     Sensor = ForeignKeyField(Sensors, on_delete='CASCADE')
     Value = CharField()
     Timestamp = DateTimeField(default=datetime.datetime.now)
@@ -57,42 +104,63 @@ class SensorValues(BaseModel):
 
     class meta:
         order_by = ('-Timestamp',)
+        # Combined index on sensor and timestamp, useful
+        # for ordering on time per sensor
         indexes = (
             (('Sensor', 'Timestamp'), True)
             )
 
 
 class SensorArgs(BaseModel):
+    """Argument of a sensor, combination of a RPCArg and a sensor
+    
+    * Sensor: The sensor for which the argument is
+    * RPCArg: The argument
+    * Value: The value of this argument
+    """
     Sensor = ForeignKeyField(Sensors, on_delete='CASCADE')
     RPCArg = ForeignKeyField(RPCArgs)
     Value = CharField()
 
 
 class Macros(BaseModel):
+    """Macro table, nothing more that key-value pairs
+    """
     Name = CharField()
     Value = CharField()
 
 
 class Match(BaseModel):
+    """Table that contains expressions used by other tables
+    
+    * Matchsting: String containing the expression
+    * Pickled: binary blob containing the pickled AST of the expression
+    """
     descr = None
     Matchstring = CharField()
     Pickled = BlobField(null=True)
 
 
 class Triggers(BaseModel):
+    """Table with triggers
+    * Name: Name of the trigger
+    * Match: foreign, match on which the trigger activates
+    * Record: Whether to log this trigger to the triggervalues table
+    * Lastvalue: Last calculated value of this trigger
+    """
     Name = CharField()
     Match = ForeignKeyField(Match)
     Record = BooleanField()
     Lastvalue = CharField(null=True, default="null")
-    
-    def last(self, num=1):
-        if self.Lastvalue:
-            return self.Lastvalue
-        else:
-            return '0'
 
 
 class TriggerValues(BaseModel):
+    """Values of triggers
+    
+    * Trigger: Trigger to which this value belongs
+    * Value: The actual value
+    * Timestamp: Time at which this value was calculated
+    """
     Trigger = ForeignKeyField(Triggers, on_delete='CASCADE')
     Value = CharField()
     Timestamp = DateTimeField(default=datetime.datetime.now)
@@ -100,12 +168,21 @@ class TriggerValues(BaseModel):
 
     class meta:
         order_by = ('-Timestamp',)
+        # Combined index on sensor and timestamp, useful
+        # for ordering on time per sensor
         indexes = (
             (('Trigger', 'Timestamp'), True)
             )
 
 
 class SensorFunctions(BaseModel):
+    """Mapping of Sensors used by expressions
+    
+    * Sensor: which sensor to which...
+    * Match: Match using this sensor
+    * Function: Which function to use on the values of this sensor
+    * Args: Tuple of arguments for the sensor function
+    """
     Sensor = ForeignKeyField(Sensors)
     Match = ForeignKeyField(Match)
     Function = CharField()
@@ -113,6 +190,13 @@ class SensorFunctions(BaseModel):
 
 
 class TriggerFunctions(BaseModel):
+    """Mapping of triggers used by expressions
+    
+    * Trigger: which trigger to which...
+    * Match: Match using this sensor
+    * Function: Which function to use on the values of this sensor
+    * Args: Tuple of arguments for the sensor function
+    """
     Trigger = ForeignKeyField(Triggers)
     Match = ForeignKeyField(Match)
     Function = CharField()
@@ -120,28 +204,57 @@ class TriggerFunctions(BaseModel):
 
 
 class Actions(BaseModel):
+    """Actions to send to a module
+    Module: The module to send the action to
+    Ident: Identifier for this action
+    """
     Module = ForeignKeyField(Module)
     ident = CharField()
 
 
 class ActionArgs(BaseModel):
+    """Arguments to send with an action RPC
+    Action: Action to which the arguments belong
+    RPCArgs: The argument
+    Value: Value of this argument
+    """
     Action = ForeignKeyField(Actions)
     RPCArg = ForeignKeyField(RPCArgs)
     Value = ForeignKeyField(Match)
 
 
 class ActionsForTrigger(BaseModel):
-    #mapping of triggers and actions
+    """Triggers to trigger an action
+
+    * Action: The action to trigger
+    * Trigger: which trigger triggers this action
+    * Match: Expression, the action is only executed 
+
+    if triggered and if the expression resolves to a non-False value
+    """
     Action = ForeignKeyField(Actions)
     Trigger = ForeignKeyField(Triggers)
     Match = ForeignKeyField(Match)
 
 
 class dbhandler:
+    """Database handler class
+    """
+
     def __init__(self, conf=None, database=None):
+        """Initialize a database connection with a specified config or database object.
+        Only specify a configuration or a database
+        
+        :param conf: Dictionary to configure a database connection with. It 
+                     should contain at least a database driver to use and 
+                     the name of the database. Other parameters are passed 
+                     to the database driver
+        :param database: Database object to use. It should be a peewee-usable database object
+        """
         self.connected = False
         if database:
-            self.databaseconn = database
+            databaseconn = database
+            dbconn.initialize(databaseconn)
         elif conf:
             try:
                 driver = conf.pop('driver')
@@ -167,6 +280,8 @@ class dbhandler:
                     raise ImproperlyConfigured("Cannot not initialize database connection")
 
     def create_tables(self):
+        """Try to create a set of empty tables, silently fails if the table already exists
+        """
         tables = [Module,
                   RPCTypes,
                   ModuleRPC,
@@ -191,6 +306,10 @@ class dbhandler:
                 pass
 
     def init_tables(self):
+        """Initialize any preconfigured content that is needed for initial 
+        start up. Currently only the RPCTypes table is filled and needed;
+        All modules are configured as inactive
+        """
         for type in rpctypes:
             try:
                 RPCTypes.get(RPCTypes.rpctype == type)
@@ -202,27 +321,38 @@ class dbhandler:
         q.execute()
 
     def connect(self):
+        """Open a connection to the database
+        """
         conn = dbconn.connect()
         self.connected = True
         return conn
 
     def close(self):
+        """Close the connection to the database
+        """
         conn = dbconn.close()
         self.connected = False
         return conn
 
     def addModule(self, name, queue, active=True):
+        """Add a module to the database.
+        
+        :param name: Name of the module
+        :param queue: queue to use at the message broker
+        :param active: Initialize the module as activated or not
+        :rtype: The created module
+        """
         return Module.create(name=name, queue=queue, Active=active)
 
     def addRPC(self, module, key, rpctype, args, descr=None):
-        '''
-            Adds an rpc command to the database
-            module: module object
-            key: name of the rpc
-            rpctype: string of the type of the rpc
-            args: list of tuples, (name, type, optional, descr)
-            descr: description of the rpc request
-        '''
+        """Adds an rpc command to the database
+        
+        :param module: module object
+        :param key: name of the rpc
+        :param rpctype: string of the type of the rpc
+        :param args: list of tuples, (name, type, optional, descr)
+        :param descr: description of the rpc request
+        """
 
         rpcrecord = RPCTypes.get(RPCTypes.rpctype == rpctype)
         with dbconn.transaction():
@@ -247,6 +377,7 @@ class dbhandler:
         
 
     def updateOrAddModule(self, moduledata):
+        
         newmodule = False
         try:
             module = Module.get(Module.name == moduledata['name'])
@@ -278,6 +409,8 @@ class dbhandler:
             pass
 
     def getdict(self, kwarg, key, value):
+        """
+        """
         if len(key.split('.', 1)) > 1:
             start, end = key.split('.', 1)
             if start not in kwarg:
@@ -308,12 +441,14 @@ class dbhandler:
         return ModuleRPC.select(ModuleRPC).join(RPCTypes).where((ModuleRPC.Module == module) & (RPCTypes.rpctype == type))
         
     def getRPCCall(self, module, type):
+        """
+        """
         return ModuleRPC.select().join(RPCTypes).where((ModuleRPC.Module == module) & (RPCTypes.rpctype == type)).limit(1)[0]
 
     def addSensor(self, module, identifier, argdata):
-        '''
+        """Add a sensor to the database
             argdata: list of dicts with name=value pairs
-        '''
+        """
         sensor = Sensors()
         sensor.ident = identifier
         sensor.Module = module
@@ -389,12 +524,16 @@ class dbhandler:
             value = TriggerValues.create(Trigger=trigger, Value=value)
     
     def checkSensorTriggers(self, sensor):
+        """
+        """
         sensortriggers = Triggers.Select().Join(Functions).Where(Functions.Sensor == sensor)
         for sensortrigger in sensortriggers:
             pass
             #check this sensor on true
 
     def getActionsfromtrigger(self, trigger_id):
+        """
+        """
         actions = ActionsForTrigger.select(ActionsForTrigger, Actions).join(Actions).where(ActionsForTrigger.Trigger == trigger_id)
         return [action for action in actions]
 
@@ -440,8 +579,11 @@ class sensorops:
     def avg(sensor, num):
         selection = sensorops._lastrecords(sensor, num)
         if last:
-            result = statistics.mean((int(i.Value) for i in selection))
-            return result
+            if statistics:
+                result = statistics.mean((int(i.Value) for i in selection))
+                return result
+            else:
+                return 0
         else:
             return 0
 
